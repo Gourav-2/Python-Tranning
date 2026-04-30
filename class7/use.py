be3d1cd89cd71265b1e87cd8602b5b3dbb0a93c4
Gourav-2 <gc27112006@gmail.com>
2026-04-30 12:55:04 +0530

    dic

--- class7/use.py
+++ class7/use.py
@@ -1,5 +1,8 @@
-import pandas as pd
-import dic.ipynb as d
+# import pandas as pd
+# import dic.ipynb as d
 
-df=pd.DataFrame(d.dic)
-print(df)
+# df=pd.DataFrame(d.dic)
+# print(df)
+
+dic={"name":["gourav","nishant","harsh"],"marks":[44,55,66]}
+print(dic["name"][0])
