8570111fde49a337d5d2cf24cd0e4c3f09977928
Gourav-2 <gc27112006@gmail.com>
2026-04-30 12:41:27 +0530

    dic

---
+++ class6/try.py
@@ -0,0 +1,19 @@
+# x="Hello everyone lets learn python"
+# y=x.split(" ")
+# for i in range(4,0,-1):
+#     print(i)
+
+# y=["hii"]
+
+# import pandas as pd
+# dic={"name":"gourav","class":"w2"}
+
+# x=pd.DataFrame("dic")
+
+# x="goourav"
+# print(x.index("o"))
+# print(x[2])
+# # ind
+# print(indices.append())
+
+import pyautogui as pt
---
+++ class7/use.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import dic.ipynb as d
+
+df=pd.DataFrame(d.dic)
+print(df)
